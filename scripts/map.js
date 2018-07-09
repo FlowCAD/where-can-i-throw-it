79a486b3d7e9eb82633c2a647bfa0ab6254cf190
FlowCAD <florian.cadoz@gmail.com>
2018-07-09 22:47:07 +0200

    Point de collecte data added but geolocalisation have an issue

--- scripts/map.js
+++ scripts/map.js
@@ -20,9 +20,10 @@ var streets = L.tileLayer(mbUrl, {id: 'mapbox.streets', attribution: mbAttr}),
     osm = L.tileLayer(osmUrl, {attribution: osmAttr});
 
 // Styles
-var alertMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-exclamation', prefix: 'fa', color: 'orange', iconColor: 'white'}),
-homeMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-home', prefix: 'fa', color: 'green', iconColor: 'white'}),
-hereMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-street-view', prefix: 'fa', color: 'orange', iconColor: 'white'});
+var trashMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-trash', prefix: 'fa', color: 'orange', iconColor: 'white'}),
+    recycleMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-recycle', prefix: 'fa', color: 'green', iconColor: 'white'}),
+    glassMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-glass', prefix: 'fa', color: 'cadetblue', iconColor: 'white'}),
+    hereMarkerSymbol = L.AwesomeMarkers.icon({icon: ' fa fa-female', prefix: 'fa', color: 'darkpurple', iconColor: 'white'});
 
 //--------------------------------------------------------------------------------------------//
 //-------------------------------------MAP INITIALIZATION-------------------------------------//
@@ -79,3 +80,99 @@ var addingData = function (layerToAdd, layerNameToAdd) {
     mymap.addLayer(layerToAdd);
     lcontrol.addOverlay(layerToAdd, layerNameToAdd);
 };
+
+//--------------------------------------------------------------------------------------------//
+//------------------------------------OPEN DATA FUNCTIONS-------------------------------------//
+//Go searching for openData from Toulouse Metropole
+var fromPointFeatureToLayer = function (featuresCreated, openDataName, openDataProperties) {
+    var myData = L.geoJson(
+        featuresCreated,
+        {
+            pointToLayer: function (feature, latlng) {
+                return new L.marker((latlng), {icon: recycleMarkerSymbol});
+            },
+            onEachFeature: function (feature, layer) {
+                var featureAttributes = "", prop, attr;
+                for (prop in openDataProperties) {
+                    for (attr in feature.properties) {
+                        if (typeof(feature.properties[attr]) !== "object" && attr === openDataProperties[prop].toLowerCase()) {
+                            featureAttributes += openDataProperties[prop] + " : " + feature.properties[attr] + "<br />";
+                        }
+                    }
+                }
+                layer.bindPopup(featureAttributes);
+            }
+        }
+    );
+    lcontrol.addOverlay(myData, openDataName);
+};
+
+var fromFeatureToFeatureType = function (featuresCreated, typeOfGeomArray, openDataName, openDataProperties) {
+    console.log("Datas to display: ", featuresCreated, typeOfGeomArray, openDataName, openDataProperties);
+    if (typeOfGeomArray.length !== 1) {
+        console.log("Il y a un problème avec le type de géométrie");
+    } else {
+        if (typeOfGeomArray[0] === "Point" || typeOfGeomArray[0] === "MultiPoint") {
+            fromPointFeatureToLayer(featuresCreated, openDataName, openDataProperties);
+        } else {
+            console.log("Il y a un problème avec le type de géométrie");
+        }
+    }
+};
+
+var FeatureConstructor = function (geometry, properties) {
+    this.geometry = geometry;
+    this.properties = properties;
+    this.type = "Feature";
+};
+
+var fromXhrToFeature = function (myResponse, openDataName, openDataProperties) {
+    var i,
+        typeOfGeomArray = [],
+        featuresCreated = {
+            "type": "FeatureCollection",
+            "features": []
+        };
+    for (i = 0; i < myResponse.records.length; i += 1) {
+        var typeOfGeom = myResponse.records[i].fields.geo_shape.type,
+            theGeom = myResponse.records[i].fields.geo_shape.coordinates,
+            featureObject = new FeatureConstructor(
+                {
+                    type: typeOfGeom,
+                    coordinates: theGeom
+                },
+                myResponse.records[i].fields
+            );
+        if (typeOfGeomArray.indexOf(typeOfGeom) === -1) {
+            typeOfGeomArray.push(typeOfGeom);
+        }
+        featuresCreated.features.push(featureObject);
+    }
+    fromFeatureToFeatureType(featuresCreated, typeOfGeomArray, openDataName, openDataProperties);
+};
+
+var myXHRSender = function (openData) {
+    var openDataLink = openData[0],
+        openDataName = openData[1],
+        openDataProperties = openData[2],
+        openDataXHR = new XMLHttpRequest();
+    openDataXHR.open('GET', openDataLink);
+    openDataXHR.send(null);
+    openDataXHR.addEventListener('readystatechange', function () {
+        if (openDataXHR.readyState === XMLHttpRequest.DONE) {
+            if (openDataXHR.status === 200) {
+                var myResponse = JSON.parse(openDataXHR.responseText);
+                fromXhrToFeature(myResponse, openDataName, openDataProperties);
+            } else {
+                console.log("openDataXHR ", openDataXHR.statusText);
+            }
+        }
+    });
+};
+
+var openDataRecupEmbal = [
+    'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=recup-emballage&geofilter.distance=43.60281%2C1.44736%2C2000&rows=100',
+    "Points de collecte",
+    ["Adresse"]
+];
+myXHRSender(openDataRecupEmbal);
